ba6e3787dd7f48b6634d054b889072d15ee18d51
Driss Kanouni <32987211+GetMetamorph@users.noreply.github.com>
2021-12-24 10:45:13 +0100

    [entities] - Added properties image to Pet entities and added faker data to PetFixtures

--- src/DataFixtures/Groups/PetFixtures.php
+++ src/DataFixtures/Groups/PetFixtures.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures\Groups;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Pet;
+use Faker\Factory;
 use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 
 class PetFixtures extends Fixture implements FixtureGroupInterface
@@ -17,14 +18,29 @@ class PetFixtures extends Fixture implements FixtureGroupInterface
     {
         $date = new \DateTime('@'.strtotime('now'));
 
+        // use the factory to create a Faker\Generator instance
+        $faker = Factory::create();
+
         //insert pets
         $species = array("Chat", "Chien", "Hamster");
         for ($i=0; $i < count($species); $i++) {  // pour chaque espèce dans l'array species
             for($j=1; $j<11; $j++){ // on fait 10 animaux different
                 $pet = new Pet();
-                $pet->setName($species[$i]." n°$j"); // ex : Chat n°1
+                $pet->setName($faker->firstName());
+                //$pet->setName($species[$i]." n°$j"); // ex : Chat n°1
                 $pet->setSpecies($species[$i]);
                 $pet->setJoinedDate($date);
+                if($species[$i] == "Chat"){
+                    $pet->setImage($faker->imageUrl(360, 360, 'animals', true, 'cats'));
+                }
+                else if($species[$i] == "Chien")
+                {
+                    $pet->setImage($faker->imageUrl(360, 360, 'animals', true, 'dogs'));
+                }
+                else if($species[$i] == "Hamster")
+                {
+                    $pet->setImage($faker->imageUrl(360, 360, 'animals', true, 'hamsters'));//hamsters n'existe peut être pas
+                }
                 $pet->setAge($j);
                 $manager->persist($pet); // create Pets
             }
