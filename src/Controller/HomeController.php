16d26ccce93a379d9069e3114e3392d030aad79a
Myhed Ben Rhouma <myhedbenrhouma@MacBook-Pro-de-Myhed.local>
2021-12-24 09:53:52 +0100

    [connect-data-repositories-with-home-controller] - add repo pet on controller + transfert datas pets to view

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,13 +6,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Repository\PetRepository;
+use Psr\Log\LoggerInterface;
+
 class HomeController extends AbstractController
 {
+    public function __construct(PetRepository $petRepos)
+    {   
+        $this->petRepos = $petRepos;   
+    }
     #[Route('/home', name: 'home')]
-    public function index(): Response
+    public function index(LoggerInterface $logger): Response
     {
+        $logger->info('toto');
+        $logger->info(json_encode($this->petRepos->findAll()));
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'pets' => $this->petRepos->findAll()
         ]);
     }
 }
